0501dc4b3544b9dcebcfd3135366ead2c772b681
Abhinav Gunishety <ag513@student.le.ac.uk>
2020-06-09 20:10:22 +0100

    Added files

---
+++ precache-manifest.2e688707f3f84507e6c4b758a8cf17fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09cdf8fef4e41a3bf6b46033085fc980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e01a57c5e3b64483d5",
+    "url": "/static/css/main.8eee8bc5.chunk.css"
+  },
+  {
+    "revision": "263045e6a1de12f0be4a",
+    "url": "/static/js/2.497c0119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.497c0119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e01a57c5e3b64483d5",
+    "url": "/static/js/main.72eea680.chunk.js"
+  },
+  {
+    "revision": "fdd2344250cabe809587",
+    "url": "/static/js/runtime-main.120dcec0.js"
+  }
+]);
